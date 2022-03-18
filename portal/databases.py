fa58f4682da7d998e64d2756a42d2a61357cca53
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-03-18 09:40:06 +0530

    auth functions

--- portal/databases.py
+++ portal/databases.py
@@ -3,6 +3,7 @@ import uuid
 from datetime import datetime
 import re
 import os
+import bcrypt
 
 
 class Database:
@@ -13,3 +14,39 @@ class Database:
         self.locations = connection['locations']
         self.admin = connection['admin']
         self.portal_reports = connection['portal_reports']
+        
+    def userExist(self, email=None, ID=None):
+        if email is not None:
+            return self.users.find_one({'email': email}) is not None
+        elif ID is not None:
+            return self.users.find_one({'_id': ID}) is not None
+        else:
+            return False
+    
+    def getUser(self, email=None, ID=None):
+        if email is not None:
+            return self.users.find_one({'email': email})
+        elif ID is not None:
+            return self.users.find_one({'_id': ID})
+        else:
+            return None
+    
+    def registerUser(self, email, password, name, role='user'):
+        if self.userExist(email=email):
+            return False
+        elif re.match(r'^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$', email) is None:
+            return False
+        else:
+            salt = bcrypt.gensalt()
+            password = bcrypt.hashpw(password.encode('utf-8'), salt)
+            self.users.insert_one({
+                '_id': str(uuid.uuid4()),
+                'email': email,
+                'password': password,
+                'name': name,
+                'role': role,
+                'created': datetime.now()
+            })
+            return True
+    
+    
